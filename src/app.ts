2adf9e6cd4d9f942c692ec5f2b1686d7b8623f81
AsZaychik <aszaychik@gmail.com>
2023-05-09 00:02:44 +0700

    Project Structure

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express, { Application, Request, Response } from 'express';
+import route from './routers/index.js';
 
 const app: Application = express();
 const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use('/', route);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
