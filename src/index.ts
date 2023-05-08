a949670ee76a07845ee4f6cde3270ab6054b6bd3
AsZaychik <aszaychik@gmail.com>
2023-05-08 23:56:44 +0700

    Test express + ts

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { num } from './num.js';
-
-const greetNum = (name: string, num: Number) => {
-  console.log(`Hi ${name.toUpperCase()}, your Number is ${num}`);
-};
-
-greetNum('Agoes', num);
