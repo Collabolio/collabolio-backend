f7e27f6b4a6497bb7667bb5636cf2c7daae338db
AsZaychik <aszaychik@gmail.com>
2023-05-11 16:23:48 +0700

    Using server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express, { Application } from 'express';
 import route from './routes/index.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app: Application = express();
-const port: number = 3000;
 
 app.use('/', route);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+export default app;
