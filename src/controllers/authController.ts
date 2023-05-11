8ea94ccbb3e006a52a8b6592ccb811cf3e2818dd
AsZaychik <aszaychik@gmail.com>
2023-05-12 06:19:18 +0700

    Register and store it in firestore

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getAuth } from 'firebase-admin/auth';
-import { CreateRequest } from '../interfaces/user';
+import admin from '../services/firebase';
 
 export const login = async (req: Request, res: Response): Promise<void> => {
   const uid = req.body.uid;
@@ -16,25 +16,36 @@ export const login = async (req: Request, res: Response): Promise<void> => {
 };
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-  const createUser: CreateRequest = {
-    email: 'test@example.com',
-    password: 'password123',
-    displayName: 'John Doe',
-    photoURL: 'https://api.dicebear.com/6.x/pixel-art/svg',
-  };
+  // const createUser: CreateRequest = {
+  //   email: 'aszaychik@example.com',
+  //   password: 'password123',
+  //   displayName: 'As Zaychik',
+  //   photoURL: 'https://api.dicebear.com/6.x/pixel-art/svg',
+  // };
   getAuth()
     .createUser({
-      email: createUser.email,
-      password: createUser.password,
+      email: req.body.email,
+      password: req.body.password,
       emailVerified: false,
-      displayName: createUser.displayName,
+      displayName: req.body.displayName,
       disabled: false,
-      photoURL: createUser.photoURL
-        ? createUser.photoURL
+      photoURL: req.body.photoURL
+        ? req.body.photoURL
         : 'https://api.dicebear.com/6.x/pixel-art/svg',
     })
     .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
       res.json(userRecord);
+      const userRef = admin.firestore().collection('users').doc(userRecord.uid);
+      return userRef.set({
+        email: userRecord.email,
+        emailVerified: userRecord.emailVerified,
+        displayName: userRecord.displayName,
+        disabled: userRecord.disabled,
+        photoURL: userRecord.photoURL,
+        isAdmin: 0,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+        deletedAt: null,
+      });
     });
 };
