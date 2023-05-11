b1bd149fc45fabe37040d54a5830f0a126f86638
AsZaychik <aszaychik@gmail.com>
2023-05-12 06:19:00 +0700

    Use firestore

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,6 +3,7 @@ import * as serviceAccount from './serviceAccountKey.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: 'https://collabolio-dev.firebaseio.com',
 });
 
 export default admin;
