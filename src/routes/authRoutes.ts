83e770881024d2f68654c5ebb2bb0f7c42cb98a7
AsZaychik <aszaychik@gmail.com>
2023-05-13 09:00:56 +0700

    just changing some list order

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import {
   getUser,
-  registerUser,
-  deleteUserAt,
   getAllUsers,
+  registerUser,
   updateUser,
+  deleteUserAt,
 } from '../controllers/authController';
 
 const router = Router();
 
 router.get('/user/:uid', getUser);
-router.post('/register', registerUser);
-router.delete('/user/:uid', deleteUserAt);
 router.get('/users', getAllUsers);
+router.post('/register', registerUser);
 router.put('/user/:uid', updateUser);
+router.delete('/user/:uid', deleteUserAt);
 
 export default router;
