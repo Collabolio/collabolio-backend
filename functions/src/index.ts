ac3a49e0df1ac8f89df5d69b205c4240ad6ced14
AsZaychik <aszaychik@gmail.com>
2023-05-13 11:14:19 +0700

    express hello world

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+
+admin.initializeApp();
+
+const app = express.default();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+export const api = functions.https.onRequest(app);
