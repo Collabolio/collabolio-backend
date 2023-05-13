b46265d55e140c11f82080e7c6b4a175ed7e100a
AsZaychik <aszaychik@gmail.com>
2023-05-13 13:44:17 +0700

    port

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,16 +5,15 @@ import cors from 'cors';
 
 import routes from './routes';
 
-export { createUserRecord } from './controllers/authController';
-export { deleteUserRecord } from './controllers/authController';
-
 admin.initializeApp();
 
 const app = express();
+const port = 3000;
 
-app.use(cors({ origin: true }));
 app.use(express.json());
+app.use(cors({ origin: true }));
 app.use('/', routes);
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
