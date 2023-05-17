2ac00770a8fcd07ff0f734602a96c1cf5780b9c7
AsZaychik <aszaychik@gmail.com>
2023-05-17 18:20:58 +0700

    Discard All

--- src/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-// Controller Function
-
-export const helloWorld = (request: Request, response: Response) => {
-  return response.json({ message: 'Hello World' });
-};
--- src/interfaces/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  email: string;
-  emailVerified?: boolean;
-  displayName: string;
-  isAdmin: number;
-  photoURL?: string;
-}
